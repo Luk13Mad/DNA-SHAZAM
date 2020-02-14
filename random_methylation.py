8862d0a1046e826417a2862a92ee066eb1bd92b5
Luk13Mad <56697676+Luk13Mad@users.noreply.github.com>
2020-02-14 14:00:22 +0100

    Create random_methylation.py
    script for random slicing and random methylation

---
+++ random_methylation.py
@@ -0,0 +1,20 @@
+from Bio import SeqIO
+import random
+import re
+from Bio.Seq import Seq
+
+gen_reads=SeqIO.parse("db278mtDNA.fasta","fasta")
+
+with open("db278mtDNA_methylated.fasta", "w+") as handle:
+    for read in gen_reads:
+        x=random.choice(range(len(read.seq)-200))
+        read.seq=read.seq[x:x+150].lower()
+        
+        ite=re.finditer(r"(?={})".format("c"),str(read.seq))
+        pos=[i.start() for i in ite]
+        idx=random.sample(pos,round(len(pos)*0.6))
+        y=list(str(read.seq))
+        for i in idx:
+            y[i]="C"
+            read.seq=Seq("".join(y))
+        SeqIO.write(read, handle,"fasta")
