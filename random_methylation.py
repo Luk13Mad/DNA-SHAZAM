3b4dda537c2a99999249928afdbc7d495fd5107e
Luk13Mad <56697676+Luk13Mad@users.noreply.github.com>
2020-03-18 15:22:05 +0100

    Update random_methylation.py
    different length

--- random_methylation.py
+++ random_methylation.py
@@ -8,11 +8,11 @@ gen_reads=SeqIO.parse("db278mtDNA.fasta","fasta")
 with open("db278mtDNA_methylated.fasta", "w+") as handle:
     for read in gen_reads:
         x=random.choice(range(len(read.seq)-200))
-        read.seq=read.seq[x:x+300].lower()
+        read.seq=read.seq[x:x+500].lower()
         
         ite=re.finditer(r"(?={})".format("c"),str(read.seq))
         pos=[i.start() for i in ite]
-        idx=random.sample(pos,round(len(pos)*0.6))
+        idx=random.sample(pos,round(len(pos)*0.4))
         y=list(str(read.seq))
         for i in idx:
             y[i]="C"
